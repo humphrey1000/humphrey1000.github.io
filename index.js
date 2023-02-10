fd1f25c7baf5a277a165cfd7f9da3679962b27c5
Humphrey Ngugi <125015338+humphrey1000@users.noreply.github.com>
2023-02-10 19:18:14 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
+let nat = document.getElementsById('body')
 let test = document.createElement('h1')
 test.innerHTML = 'welcome to my repo'
-let nat = document.getElementsByTagName('body')
 nat.appendChild(test)
